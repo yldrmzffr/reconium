883c8b859d9458294fbd785c9e7663301b34844f
muzaffer yıldırım <muzaffer@yldrmzffr.com>
2019-06-27 21:29:27 +0300

    v2

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'reconium',
+  packages = ['reconium'],
+  version = '0.2',
+  description = 'record selenium test video',
+  author = 'yldrmzffr',
+  author_email = 'muzaffer@yldrmzffr.com',
+  url = 'https://github.com/yldrmzffr/reconium',
+  download_url = 'https://github.com/yldrmzffr/reconium/tarball/0.2',
+  keywords = ['selenium', 'video', 'recorder'], 
+  classifiers = [],
+)
